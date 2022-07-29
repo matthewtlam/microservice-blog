1aaca52206e40bf8ad7f858e734bf27c01e49299
Matthew Lam <m39lam@uwaterloo.ca>
2022-07-29 00:24:50 -0400

    Creating query microservice

---
+++ event-bus/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    // Posts
+    axios.post('http://localhost:4000/events', event).catch((err) => {
+        console.log(err.message);
+    });
+
+    // Comments
+    axios.post('http://localhost:4001/events', event).catch((err) => {
+        console.log(err.message);
+    });
+
+    // Query
+    axios.post('http://localhost:4002/events', event).catch((err) => {
+        console.log(err.message);
+    });
+
+    res.send({status: 'OK'});
+
+});
+
+app.listen(4005, () => {
+    console.log('Listening on 4005');
+});
